9dcc26923b91af2cbd5fead215a8fcec0fbd80af
dhavalptl630007 <dhavalptl630007@gmail.com>
2020-08-25 20:45:00 -0400

    First commit goldebbites POS

---
+++ src/main/java/com/goldenbites/pos/GoldenbitesApplication.java
@@ -0,0 +1,13 @@
+package com.goldenbites.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenbitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenbitesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldenbites/pos/GoldenbitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenbites.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenbitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
