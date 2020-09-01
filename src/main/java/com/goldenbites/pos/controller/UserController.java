3b2e21833e2a7c4df9027faeaa6225bd017ea1f5
TirthShah510 <tirthshah510@gmail.com>
2020-09-01 17:24:37 -0400

    user controller added

--- src/main/java/com/goldenbites/pos/controller/UserController.java
+++ src/main/java/com/goldenbites/pos/controller/UserController.java
@@ -1,26 +1,38 @@
 package com.goldenbites.pos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.goldenbites.pos.dao.UserRepository;
 import com.goldenbites.pos.model.User;
 
 @Controller
 public class UserController {
 
-	@GetMapping("/user")
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/login")
 	public String greetingForm(Model model) {
 		model.addAttribute("user", new User());
-		return "user";
+		return "login";
 	}
 
-	@PostMapping("/user")
-	public String greetingSubmit(@ModelAttribute User user, Model model) {
-		model.addAttribute("user", user);
-		return "result";
+	@PostMapping("/login")
+	public String greetingSubmit(@ModelAttribute User user) {
+		
+		User userNew = userRepository.findByuserName(user.getUserName());
+
+		
+		if(userNew !=  null && userNew.getUserPassword().equals(user.getUserPassword()))
+			return "index";
+		else
+			return "login";
+			
 	}
 
 }
