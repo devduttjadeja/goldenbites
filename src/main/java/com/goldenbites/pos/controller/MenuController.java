5cfae6cff8ba7288380643a9734ecb260bd069bf
dhavalptl630007 <dhavalptl630007@gmail.com>
2020-09-04 19:20:06 -0400

    view items

--- src/main/java/com/goldenbites/pos/controller/MenuController.java
+++ src/main/java/com/goldenbites/pos/controller/MenuController.java
@@ -1,24 +1,52 @@
 package com.goldenbites.pos.controller;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.goldenbites.pos.dao.ItemRepository;
-import com.goldenbites.pos.dao.OrderRepository;
-
+import com.goldenbites.pos.model.Item;
+import com.goldenbites.pos.model.User;
 
 @Controller
-
 public class MenuController {
 
 	@Autowired
-	  ItemRepository itemRepository;
-	
-	@GetMapping("/menu")
-	public String categoryDisplay(Model model) {
+	ItemRepository itemRepository;
+
+	@GetMapping("/viewitems")
+	public String viewItems(Model model) {
 		model.addAttribute("items", itemRepository.findAll());
-		return "Menu/menu";
+		return "Menu/viewItems";
+	}
+
+	@GetMapping("/menuhome")
+	public String viewMenu(Model model) {
+		// model.addAttribute("items", itemRepository.findAll());
+		return "Menu/menuhome";
+	}
+
+	@GetMapping("/additem")
+	public String displayItemForm(Model model) {
+		//model.addAttribute("items", item);
+		model.addAttribute("item", new Item());
+		return "Menu/additem";
+	}
+	@PostMapping("/additem")
+	public String saveItem(@ModelAttribute Item item, Model model) {
+		//model.addAttribute("items", item);
+		Calendar calendar = Calendar.getInstance();
+		Date now = calendar.getTime();
+	    item.setItemCreatedDate(now);
+		itemRepository.save(item);
+		model.addAttribute("item", new Item());
+		return "Menu/additem";
 	}
 }
