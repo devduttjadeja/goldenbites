f82745e382a632305e09e7f8ce79ee4d9faf57a4
dhavalptl630007 <dhavalptl630007@gmail.com>
2020-09-21 21:20:32 -0400

    view orders features added

--- src/main/java/com/goldenbites/pos/controller/OrderController.java
+++ src/main/java/com/goldenbites/pos/controller/OrderController.java
@@ -1,24 +1,23 @@
 package com.goldenbites.pos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.goldenbites.pos.dao.OrderRepository;
 
-@RestController
+@Controller
 public class OrderController {
 
 	  @Autowired
 	  OrderRepository orderRepository;
 
-//	  @PostMapping("/order")
-//	  public ResponseEntity<Order> createOrder(@RequestBody Order order) {
-//	    try {
-//	      Order _order = orderRepository.save(new Order(order.getDate(), order.getTotal()));
-//	      return new ResponseEntity<>(_order, HttpStatus.CREATED);
-//	    } catch (Exception e) {
-//	      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//	    }
-//	  }
+		@GetMapping("/viewOrders")
+		public String viewOrders(Model model) {
+			model.addAttribute("orders", orderRepository.findAll());
+			return "Order/viewOrders";
+		}
+	  
 	
 }
